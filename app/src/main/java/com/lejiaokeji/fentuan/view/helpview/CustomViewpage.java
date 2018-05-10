135eef60d4b6b3206d69e7de5a0a556c0218fe0c
zouyuanjun <zouyuanjunli@foxmail.com>
2018-05-10 15:36:58 +0800

    恢复

--- app/src/main/java/com/lejiaokeji/fentuan/view/helpview/CustomViewpage.java
+++ app/src/main/java/com/lejiaokeji/fentuan/view/helpview/CustomViewpage.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 public class CustomViewpage extends ViewPager {
+    private boolean isScroll;
     public CustomViewpage(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
@@ -14,19 +15,49 @@ public class CustomViewpage extends ViewPager {
         super(context);
     }
 
+    /**
+     * 1.dispatchTouchEvent一般情况不做处理
+     *,如果修改了默认的返回值,子孩子都无法收到事件
+     */
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        //不拦截,否则子孩子都无法收到事件,一般这个自定义的时候都不作处理
-        return super.dispatchTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);   // return true;不行
     }
-
+    /**
+     * 是否拦截
+     * 拦截:会走到自己的onTouchEvent方法里面来
+     * 不拦截:事件传递给子孩子
+     */
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return super.onInterceptTouchEvent(ev);
+        // return false;//可行,不拦截事件,
+        // return true;//不行,孩子无法处理事件
+        //return super.onInterceptTouchEvent(ev);//不行,会有细微移动
+        if (isScroll){
+            return super.onInterceptTouchEvent(ev);
+        }else{
+            return false;
+        }
     }
-
+    /**
+     * 是否消费事件
+     * 消费:事件就结束
+     * 不消费:往父控件传
+     */
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        return true;
+        //return false;// 可行,不消费,传给父控件
+        //return true;// 可行,消费,拦截事件
+        //super.onTouchEvent(ev); //不行,
+        //虽然onInterceptTouchEvent中拦截了,
+        //但是如果viewpage里面子控件不是viewgroup,还是会调用这个方法.
+        if (isScroll){
+            return super.onTouchEvent(ev);
+        }else {
+            return true;// 可行,消费,拦截事件
+        }
+    }
+    public void setScroll(boolean scroll) {
+        isScroll = scroll;
     }
 }
